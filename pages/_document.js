4de7a5ea79094813d9d59eee4ca26ae312418d82
Shivansh Soni <shivanshsoni@berkeley.edu>
2024-11-01 16:22:30 -0700

    updated proj structure

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Preconnect and load your font here */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
